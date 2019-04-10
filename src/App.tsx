9711cbea4da49e0ccb905495202d6a1d628137dc
Francis Masha <francismasha96@gmail.com>
2019-04-10 10:40:33 +0300

    mdc-textfield #001

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TextField, {HelperText, Input} from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import '@material/react-text-field/dist/text-field.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [username, setUsername] = useState('');
+
+  const handleValueChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value)
+  };
+
+  return (
+    <div>
+      <TextField
+        outlined
+        label='Username'
+        trailingIcon={<MaterialIcon role="button" icon="face" initRipple={null}/>}
+        helperText={<HelperText>Kindly fill in the username</HelperText>}
+      >
+        <Input
+          value={username}
+          required={true}
+          onChange={handleValueChange}/>
+      </TextField>
+    </div>
+  );
 }
 
 export default App;
